40317b4a50891c0deeb0b6e037a0c5e14440e534
smaq92 <145607578+smaq92@users.noreply.github.com>
2025-04-01 11:20:07 -0600

    modify boiler plate

--- app/eslint.config.js
+++ app/eslint.config.js
@@ -1,6 +1,7 @@
 import js from '@eslint/js'
 import pluginVue from 'eslint-plugin-vue'
 import globals from 'globals'
+import skipFormatting from '@vue/eslint-config-prettier/skip-formatting'
 
 export default [
   {
@@ -23,4 +24,5 @@ export default [
 
   js.configs.recommended,
   ...pluginVue.configs['flat/essential'],
+  skipFormatting,
 ]
