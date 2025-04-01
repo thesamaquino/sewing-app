43a516a9c9727fecb0efb6d4726daaa0adaa5293
smaq92 <145607578+smaq92@users.noreply.github.com>
2025-04-01 11:33:06 -0600

    change read me

--- .history/app/src/main_20250401111820.js
+++ .history/app/src/main_20250401111820.js
@@ -1,4 +1,3 @@
-// root component
 import './assets/main.css'
 
 import { createApp } from 'vue'
@@ -7,12 +6,9 @@ import { createPinia } from 'pinia'
 import App from './App.vue'
 import router from './router'
 
-// creating app
 const app = createApp(App)
 
-// telling app to use pinia and router
 app.use(createPinia())
 app.use(router)
 
-// we are mounting the app
 app.mount('#app')
